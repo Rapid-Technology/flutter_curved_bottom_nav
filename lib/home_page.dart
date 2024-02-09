8e4a6c88b2a228b674424637f6bd5514516fcf43
Yasin Mohammadi <yasinm289@gmail.com>
2024-02-10 00:13:21 +0430

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _page = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: Colors.green,
+        color: Colors.green,
+        animationDuration: const Duration(milliseconds: 300),
+        items: const <Widget>[
+          Icon(Icons.home, size: 26, color: Colors.white),
+          Icon(Icons.message, size: 26, color: Colors.white),
+          Icon(Icons.add, size: 26, color: Colors.white),
+          Icon(Icons.notifications, size: 26, color: Colors.white),
+          Icon(Icons.person, size: 26, color: Colors.white),
+        ],
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+      body: Center(
+        child: Text(
+          _page.toString(),
+          style: const TextStyle(
+            fontSize: 100,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
